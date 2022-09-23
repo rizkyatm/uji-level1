f9ee523a69bba9db3639265155bc7503ddc5d0cd
rizkyatm <rizkyatmajaatm@gmail.com>
2022-09-23 08:09:27 +0700

    hari-ketiga dan keempat

--- tampilandatasiswa.php
+++ tampilandatasiswa.php
@@ -4,6 +4,7 @@ include 'koneksi.php';
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -13,51 +14,57 @@ include 'koneksi.php';
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 </head>
-<body>
-<div class="bdytambah">
-    <div class="bar">
-        <div class="profil">
-            <div class="logo"><img src="./images/LSTB-removebg-preview.png" alt="logo"></div>
-            <h2>SARPRAS</h2>
-        </div>
-        <div class="dmaster">
-            <a href="#" class="d-siswa"><i class="fas fa-user-friends" style="font-size:20px;"></i>  Data siswa</a>
-            <a href="tampildatabarang.php" class="d-barang"><i class="fas fa-box" style="font-size:21px;"></i>  Data barang</a>
-        </div>
-    </div>
 
-    <div class="tbsiswa">
-    <h3>Data Siswa</h3>
-    <h4><a href="tambahdatasiswa.html">Tambah Baru</a></h4>
-    <div class="a-table">
-    <table border="1">
-        <tr>
-            <th>NISN</th>
-            <th>Nama Siswa</th>
-            <th>Kelas</th>
-            <th>Agama</th>
-            <th>jenis Kelamin</th>
-            <th colspan="2" style="padding-left:32px;">Action</th>
-        </tr>
-        <?php
-            $sql = "SELECT * FROM data_siswa";
-            $query = mysqli_query ($connect,$sql);
-            while($sis = mysqli_fetch_array ($query)){
-                echo "
-            <tr>
-            <td>$sis[nisn]</td>
-            <td>$sis[nama]</td>
-            <td>$sis[kelas]</td>
-            <td>$sis[agama]</td>
-            <td>$sis[jenis_kelamin]</td>
-            <td><a href='formeditsiswa.php?nisn=".$sis['nisn']."'<input type='button' class='btn-edit'></a></td>
-            <td><a href='hapusdatasiswa.php?nisn=".$sis['nisn']."'<input type='button' class='btn-hapus'></a> </td>
-            </tr>";
-            }
-        ?>
-    </table>
-    </div>
-    </div>
+<body>
+    <div class="bdytambah">
+        <div class="bar">
+            <div class="profil">
+                <div class="logo"><img src="./images/LSTB-removebg-preview.png" alt="logo"></div>
+                <h2>SARPRAS</h2>
+            </div>
+            <div class="dmaster">
+                <a href="#" class="d-siswa"><i class="fas fa-user-friends" style="font-size:20px;"></i> Data siswa</a>
+                <a href="tampildatabarang.php" class="d-barang"><i class="fas fa-box" style="font-size:21px;"></i> Data barang</a>
+            </div>
+        </div>
+
+        <div class="tbsiswa">
+            <h3>Data Siswa</h3>
+            <h4><a href="tambahdatasiswa.html">Tambah Baru</a></h4>
+            <div class="a-table">
+                <table>
+                    <tr>
+                        <th>No</th>
+                        <th>NISN</th>
+                        <th>Nama Siswa</th>
+                        <th>Kelas</th>
+                        <th>Agama</th>
+                        <th>jenis Kelamin</th>
+                        <th colspan="2" style="padding-left:32px;">Action</th>
+                    </tr>
+                    <?php
+                    $no = 1;
+                    $sql = "SELECT * FROM data_siswa";
+                    $query = mysqli_query($connect, $sql);
+                    while ($sis = mysqli_fetch_array($query)) {
+                        echo "
+                    <tr>
+                    <td>$no</td>
+                    <td>$sis[nisn]</td>
+                    <td>$sis[nama]</td>
+                    <td>$sis[kelas]</td>
+                    <td>$sis[agama]</td>
+                    <td>$sis[jenis_kelamin]</td>
+                    <td><a href='formeditsiswa.php?nisn=".$sis['nisn']."'<input type='button' class='btn-edit'></a></td>
+                    <td><a href='hapusdatasiswa.php?nisn=".$sis['nisn']."'<input type='button' class='btn-hapus'></a> </td>
+                    </tr>";
+                        $no++;
+                    }
+                    ?>
+                </table>
+            </div>
+        </div>
     </div>
 </body>
+
 </html>
--- tampildatabarang.php
+++ tampildatabarang.php
@@ -4,6 +4,7 @@ include 'koneksi.php';
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -12,51 +13,57 @@ include 'koneksi.php';
     <link rel="stylesheet" href="barang.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
-<body>
-<div class="bdytambah">
-<div class="bar">
-        <div class="profil">
-            <div class="logo"><img src="./images/LSTB-removebg-preview.png" alt="logo"></div>
-            <h2>SARPRAS</h2>
-        </div>
-        <div class="dmaster">
-            <a href="tampilandatasiswa.php  " class="d-siswa"><i class="fas fa-user-friends" style="font-size:20px;"></i>  Data siswa</a>
-            <a href="#" class="d-barang"><i class="fas fa-box" style="font-size:21px;"></i>  Data barang</a>
-        </div>
-    </div> 
 
-    <div class="tbbarang">
-        <h3>Data Barang</h3>
-        <h4><a href="tambahdatabarang.html">Tambah Baru</a></h4>
-        <div>
-        <table border="">
-        <tr>
-            <th>Kode Barang</th>
-            <th>Nama Barang</th>
-            <th>Jumlah</th>
-            <th>Satuan</th>
-            <th>jenis Barang</th>
-            <th colspan="2" style="padding-left:25px;">Action</th>
-        </tr>
-        <?php
-            $sql = "SELECT * FROM data_barang";
-            $query = mysqli_query ($connect,$sql);
-            while($sis = mysqli_fetch_array ($query)){
-                echo "
-            <tr>
-            <td>$sis[kode_barang]</td>
-            <td>$sis[nama_barang]</td>
-            <td>$sis[jumlah_barang]</td>
-            <td>$sis[satuan]</td>
-            <td>$sis[jenis_barang]</td>
-            <td><a href='formeditbarang.php?kode_barang=".$sis['kode_barang']."'<input type='button' class='btn-edit'></a></td>
-            <td><a href='hapusdatabarang.php?kode_barang=".$sis['kode_barang']."'<input type='button' class='btn-hapus'></a></td>
-            </tr>";
-            }
-        ?>
-    </table>
+<body>
+    <div class="bdytambah">
+        <div class="bar">
+            <div class="profil">
+                <div class="logo"><img src="./images/LSTB-removebg-preview.png" alt="logo"></div>
+                <h2>SARPRAS</h2>
+            </div>
+            <div class="dmaster">
+                <a href="tampilandatasiswa.php  " class="d-siswa"><i class="fas fa-user-friends" style="font-size:20px;"></i> Data siswa</a>
+                <a href="#" class="d-barang"><i class="fas fa-box" style="font-size:21px;"></i> Data barang</a>
+            </div>
+        </div>
+
+        <div class="tbbarang">
+            <h3>Data Barang</h3>
+            <h4><a href="tambahdatabarang.html">Tambah Baru</a></h4>
+            <div>
+                <table>
+                    <tr>
+                        <th>No</th>
+                        <th>Kode</th>
+                        <th>Nama Barang</th>
+                        <th>Jumlah</th>
+                        <th>Satuan</th>
+                        <th>jenis Barang</th>
+                        <th colspan="2" style="padding-left:25px;">Action</th>
+                    </tr>
+                    <?php
+                    $no = 1;
+                    $sql = "SELECT * FROM data_barang";
+                    $query = mysqli_query($connect, $sql);
+                    while ($sis = mysqli_fetch_array($query)) {
+                        echo "
+                    <tr>
+                    <td>$no</td>
+                    <td>$sis[kode_barang]</td>
+                    <td>$sis[nama_barang]</td>
+                    <td>$sis[jumlah_barang]</td>
+                    <td>$sis[satuan]</td>
+                    <td>$sis[jenis_barang]</td>
+                    <td><a href='formeditbarang.php?kode_barang=" . $sis['kode_barang'] . "'<input type='button' class='btn-edit'></a></td>
+                    <td><a href='hapusdatabarang.php?kode_barang=" . $sis['kode_barang'] . "'<input type='button' class='btn-hapus'></a></td>
+                    </tr>";
+                        $no++;
+                    }
+                    ?>
+                </table>
+            </div>
+        </div>
     </div>
-    </div>
-    </div>   
 </body>
+
 </html>
